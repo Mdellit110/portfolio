56d5bad78634c3a7b9bd31edb5bb9dd2e91c3e01
matthew dellitalia <matthewdellitalia@matthews-mbp-2.lan>
2019-04-24 00:53:28 -0400

    portfolio section scrolls

--- src/components/ContactPage.jsx
+++ src/components/ContactPage.jsx
@@ -1,12 +1,47 @@
-import React from 'react';
+import React from "react";
+
 
 const ContactPage = (props) => {
   const {contact} = props
   return (
-    <div ref={contact} className='page'>
-      <h1>Contact Me</h1>
+    <div ref={contact} className="page">
+      <div className="tag">
+        <h1>Contact Me</h1>
+      </div>
+      <div className="contact-page">
+        <form className="contact-form"
+              id="contactform"
+              action="//formspree.io/matt.dellitalia@gmail.com"
+              method="POST">
+          <div className='form-container'>
+            <div className='form-input'>
+              <input
+                type="text"
+                name="name"
+                placeholder="Enter your name here"/>
+            </div>
+            <div className='form-input'>
+              <input
+                type="email"
+                name="_replyto"
+                placeholder="Enter an email address I can get back to you at here"/>
+            </div>
+            <div className='form-input'>
+              <textarea
+                name="comments"
+                placeholder="say something here!"/>
+            </div>
+            <div className='form-input'>
+              <input
+                className="contact-input"
+                type="submit"
+                value="Send"/>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   )
-}
+};
 
 export default ContactPage;
--- src/components/ProjectPage.jsx
+++ src/components/ProjectPage.jsx
@@ -1,52 +1,46 @@
 import React from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
+import github from '../images/github-brands.svg';
 
 const ProjectPage = (props) => {
   const {portfolio} = props
   return (
     <div ref={portfolio} className='page'>
-      <h1>Portfolio</h1>
+      <div className="tag">
+        <h1>Portfolio</h1>
+      </div>
       <div className="portfolio-page">
-        <div className="carousel-container">
-          <Carousel>
-            <div>
-              <img src="https://i.imgur.com/5QGtW1R.png" />
-              <div className="legend" style={{padding: 0}}>
-                <a className='carousel-link' href="http://tradesync.surge.sh" target="_blank">Link to TradeSync</a>
-                <a className='carousel-link' href="https://github.com/Mdellit110/TradeSync" target="_blank">Link to Github</a>
+            <div className="project-card1">
+              <div className="portfolio-links">
+                <a className="site" href="http://tradesync.surge.sh" target="_blank">Live Site</a>
+                <a href="https://github.com/Mdellit110/TradeSync" target="_blank"><img src={github} alt="github"/></a>
               </div>
             </div>
-            <div>
-              <img src="https://i.imgur.com/LMeLl2S.png" />
-              <div className="legend" style={{padding: 0}}>
-                <a className='carousel-link' href="http://dodgyblobby.surge.sh" target="_blank">Link to Dodgy Blobby</a>
-                <a className='carousel-link' href="https://github.com/Mdellit110/ESCAPE-the-game" target="_blank">Link to Github</a>
+            <div className="project-card2">
+              <div className="portfolio-links">
+                <a className="site" href="http://dodgyblobby.surge.sh" target="_blank">Live Site</a>
+                <a href="https://github.com/Mdellit110/ESCAPE-the-game" target="_blank"><img src={github} alt="github"/></a>
               </div>
             </div>
-            <div>
-              <img src="https://i.imgur.com/hrGbwkI.png" />
-              <div className="legend" style={{padding: 0}}>
-                <a className='carousel-link' href="https://walkordrive.netlify.com" target="_blank">Link to Walk or Drive</a>
-                <a className='carousel-link' href="https://github.com/Mdellit110/Project2" target="_blank">Link to Github</a>
+            <div className="project-card3">
+              <div className="portfolio-links">
+                <a className="site" href="https://walkordrive.netlify.com" target="_blank">Live Site</a>
+                <a href="https://github.com/Mdellit110/Project2" target="_blank"><img src={github} alt="github"/></a>
               </div>
             </div>
-            <div>
-              <img src="https://i.imgur.com/t0ArjBI.png" />
-              <div className="legend" style={{padding: 0}}>
-                <a className='carousel-link' href="https://subrats.netlify.com" target="_blank">Link to SubRats</a>
-                <a className='carousel-link' href="https://github.com/Mdellit110/subrats" target="_blank">Link to Github</a>
+            <div className="project-card4">
+              <div className="portfolio-links">
+                <a className="site" href="https://subrats.netlify.com" target="_blank">Live Site</a>
+                <a href="https://github.com/Mdellit110/subrats" target="_blank"><img src={github} alt="github"/></a>
               </div>
             </div>
-            <div>
-              <img src="https://i.imgur.com/ImgpX39.png" />
-              <div className="legend" style={{padding: 0}}>
-                <a className='carousel-link' href="https://everybodyeats.netlify.com" target="_blank">Link to Everybody Eats</a>
-                <a className='carousel-link' href="https://github.com/Mdellit110/Everybody-Eats" target="_blank">Link to Github</a>
+            <div className="project-card5">
+              <div className="portfolio-links">
+                <a className="site" href="https://everybodyeats.netlify.com" target="_blank">Live Site</a>
+                <a href="https://github.com/Mdellit110" target="_blank"><img src={github} alt="github"/></a>
               </div>
             </div>
-          </Carousel>
-        </div>
       </div>
     </div>
   )
