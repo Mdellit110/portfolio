1f88bc0a63a31c43d4f3b08456301c33999a7e40
matthew dellitalia <matthewdellitalia@matthews-mbp-2.lan>
2019-05-17 13:00:06 -0400

    made mobile responsive

--- src/components/ContactPage.jsx
+++ src/components/ContactPage.jsx
@@ -1,47 +1,44 @@
 import React from "react";
 
-
-const ContactPage = (props) => {
-  const {contact} = props
+const ContactPage = props => {
+  const { contact } = props;
   return (
     <div ref={contact} className="page">
       <div className="tag">
         <h1>Contact Me</h1>
       </div>
       <div className="contact-page">
-        <form className="contact-form"
-              id="contactform"
-              action="//formspree.io/matt.dellitalia@gmail.com"
-              method="POST">
-          <div className='form-container'>
-            <div className='form-input'>
-              <input
-                type="text"
-                name="name"
-                placeholder="Enter your name here"/>
+        <form
+          className="contact-form"
+          id="contactform"
+          action="//formspree.io/matt.dellitalia@gmail.com"
+          method="POST"
+        >
+          <div className="form-container">
+            <div className="form-input">
+              <input type="text" name="name" placeholder="Whats your name?" />
             </div>
-            <div className='form-input'>
+            <div className="form-input">
               <input
                 type="email"
                 name="_replyto"
-                placeholder="Enter an email address I can get back to you at here"/>
+                placeholder="Put your email adress here!"
+              />
             </div>
-            <div className='form-input'>
+            <div className="form-input">
               <textarea
                 name="comments"
-                placeholder="say something here!"/>
+                placeholder="And your message right here!"
+              />
             </div>
-            <div className='form-input'>
-              <input
-                className="contact-input"
-                type="submit"
-                value="Send"/>
+            <div className="form-input">
+              <input className="contact-input" type="submit" value="Send" />
             </div>
           </div>
         </form>
       </div>
     </div>
-  )
+  );
 };
 
 export default ContactPage;
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from "react";
 
-const HomePage = (props) => {
-  const {toggle, home} = props
+const HomePage = props => {
+  const { toggle, home } = props;
   return (
     <div className={home} onClick={toggle}>
       <div className="home-content">
         <h1>Hi, I'm Matt, and I enjoy being constantly challenged </h1>
-        <div className='to-portfolio'>checkout my latest projects!</div>
+        <div className="to-portfolio">
+          click anywhere to checkout my latest projects!
+        </div>
       </div>
-  </div>
-  )
-}
+    </div>
+  );
+};
 
 export default HomePage;
