79b796293c21ffd4dd5a97e5eb0e09b82cda2f7c
matthew dellitalia <matthewdellitalia@matthews-mbp-2.lan>
2019-05-17 22:08:01 -0400

    add isMobile function

--- src/App.js
+++ src/App.js
@@ -1,64 +1,76 @@
-import React, { Component } from 'react';
-import './styles/App.css';
-import './styles/nav.css';
-import './styles/home.css';
-import './styles/content.css';
-import './styles/about.css';
-import './styles/portfolio.css';
-import './styles/contact.css';
-import NavBar from './components/NavBar';
-import HomePage from './components/HomePage';
-import Content from './components/Content';
+import React, { Component } from "react";
+import "./styles/App.css";
+import "./styles/nav.css";
+import "./styles/home.css";
+import "./styles/content.css";
+import "./styles/about.css";
+import "./styles/portfolio.css";
+import "./styles/contact.css";
+import NavBar from "./components/NavBar";
+import HomePage from "./components/HomePage";
+import Content from "./components/Content";
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      is_home: 'home-page',
-    }
-    this.about = React.createRef()
-    this.portfolio = React.createRef()
-    this.contact = React.createRef()
-    this.toggleHome = this.toggleHome.bind(this)
-    this.goTo = this.goTo.bind(this)
+      is_home: "home-page"
+    };
+    this.about = React.createRef();
+    this.portfolio = React.createRef();
+    this.contact = React.createRef();
+    this.toggleHome = this.toggleHome.bind(this);
+    this.goTo = this.goTo.bind(this);
+    this.isMobileDevice = this.isMobileDevice.bind(this);
   }
-
+  isMobileDevice() {
+    return (
+      typeof window.orientation !== "undefined" ||
+      navigator.userAgent.indexOf("IEMobile") !== -1
+    );
+  }
   toggleHome() {
-    if (this.state.is_home === 'home-page') {
+    if (this.state.is_home === "home-page") {
       this.setState(prevState => ({
-        is_home: 'hidden'
-      }))
+        is_home: "hidden"
+      }));
     } else {
       this.setState(prevState => ({
-        is_home: 'home-page'
-      }))
+        is_home: "home-page"
+      }));
     }
   }
 
   goTo(ref) {
-    if (ref === 'about') {
-      this.about.current.scrollIntoView({ behavior: 'smooth', block: 'start' })
-    } else if (ref === 'portfolio'){
-      this.portfolio.current.scrollIntoView({ behavior: 'smooth', block: 'start' })
-    } else if (ref === 'contact'){
-      this.contact.current.scrollIntoView({ behavior: 'smooth', block: 'start' })
+    if (ref === "about") {
+      this.about.current.scrollIntoView({ behavior: "smooth", block: "start" });
+    } else if (ref === "portfolio") {
+      this.portfolio.current.scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+      });
+    } else if (ref === "contact") {
+      this.contact.current.scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+      });
     }
     console.log(ref);
   }
-
 
   render() {
     return (
       <div className="App">
         <HomePage
           toggle={this.toggleHome}
-          home={this.state.is_home}/>
-        <NavBar
-          toggle={this.toggleHome}
-          goTo={this.goTo}/>
+          home={this.state.is_home}
+          isMobile={this.isMobileDevice}
+        />
+        <NavBar toggle={this.toggleHome} goTo={this.goTo} />
         <Content
           about={this.about}
           portfolio={this.portfolio}
-          contact={this.contact}/>
+          contact={this.contact}
+        />
       </div>
     );
   }
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect } from "react";
 import Hammer from "@egjs/hammerjs";
 const HomePage = props => {
-  const { toggle, home } = props;
+  const { toggle, home, isMobile } = props;
+
   function setUpHammer() {
     let homePage = document.querySelector(".home-page");
     let manager = new Hammer.Manager(homePage);
@@ -10,25 +11,26 @@ const HomePage = props => {
     return manager;
   }
   useEffect(() => {
-    let deltaY = 0;
-    const manager = setUpHammer();
-    manager.on("swipe", e => {
-      deltaY = deltaY + e.deltaY;
-      let direction = e.offsetDirection;
-      let translate3d = `translate3d ${deltaY}px, 0, 0)`;
-      if (deltaY < 0) {
+    console.log(isMobile());
+    if (isMobile) {
+      let deltaY = 0;
+      const manager = setUpHammer();
+      manager.on("swipeup", e => {
+        console.log("swipeup");
         toggle();
-      }
-      deltaY = 0;
-    });
+        manager.off();
+      });
+    }
   }, []);
 
   return (
-    <div className={home} onClick={toggle}>
+    <div className={home} onClick={isMobile() ? "" : toggle}>
       <div className="home-content">
         <h1>Hi, I'm Matt, and I enjoy being constantly challenged </h1>
         <div className="to-portfolio">
-          click anywhere to checkout my latest projects!
+          {isMobile()
+            ? `swipe up to checkout my latest projects!`
+            : `click anywhere to checkout my latest projects!`}
         </div>
       </div>
     </div>
